12e4f17d2847d26f2ef8144813b80e69e60c9238
Swapnil Sable <ssable@gammastack.com>
2020-12-02 00:20:15 +0530

    Use on liner return syntax for arrow function to implement stateless components.

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import Option from './Option';
 
-const Options = (props) => {
-  return (
-    <div>
-      {props.options.length === 0 && <p>The options list is empty now.</p>}
-      <button onClick={props.handleRemoveOptions}>Remove All</button>
-      {
-        props.options.map((option) => (
-          <Option key={option}
-            optionText={option}
-            handleRemoveOption={props.handleRemoveOption} />
-        ))
-      }
-    </div>
-  );
-}
+const Options = (props) => (
+  <div>
+    {props.options.length === 0 && <p>The options list is empty now.</p>}
+    <button onClick={props.handleRemoveOptions}>Remove All</button>
+    {
+      props.options.map((option) => (
+        <Option key={option}
+          optionText={option}
+          handleRemoveOption={props.handleRemoveOption} />
+      ))
+    }
+  </div>
+);
 
 export default Options;
