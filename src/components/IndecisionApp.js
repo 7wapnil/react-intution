30fa0654e13de2ddcd467f0273a12d7907cd778f
Swapnil Sable <ssable@gammastack.com>
2020-12-03 03:02:43 +0530

    Style options, option and add option component.

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -75,12 +75,14 @@ export default class IndecisionApp extends React.Component {
               hasOptions={this.state.options.length > 0}
               handlePick={this.handlePick}
             />
-            <Options
-              options={this.state.options}
-              handleRemoveOptions={this.handleRemoveOptions}
-              handleRemoveOption={this.handleRemoveOption}
-            />
-            <AddOption handleAddOption={this.handleAddOption} />
+            <div className="widget">
+              <Options
+                options={this.state.options}
+                handleRemoveOptions={this.handleRemoveOptions}
+                handleRemoveOption={this.handleRemoveOption}
+              />
+              <AddOption handleAddOption={this.handleAddOption} />
+            </div>
           </div>
         <OptionModal
           selectedOption={this.state.selectedOption}
